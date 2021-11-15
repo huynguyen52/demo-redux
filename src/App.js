0069cd4a4226a114f0b7566eb45d83b8e84252b6
huynguyen <huynguyen5212@gmail.com>
2021-11-15 17:55:03 +0700

    comment all page

--- src/App.js
+++ src/App.js
@@ -15,21 +15,21 @@ import PostPage from "./pages/BlogPage/PostPage";
 function App() {
   return (
     <div className="App">
-      {/* <Overlay />
+      <Overlay />
       <HeaderMain />
       <div className="container">
         <HeaderBottom />
       </div>
-      <Switch>
+      {/* <Switch>
         <Route path="/" exact component={HomePage} />
         <Route path="/product" component={ProductPage} />
         <Route path="/shop" component={ShopPage} />
         <Route path="/cart" component={CartPage} />
         <Route path="/blog" component={PostPage} />
-      </Switch>
+      </Switch> */}
       <div className="top-div" style={{ borderTop: "1px solid #000000" }}></div>
       <Footer />
-      <ToTopButton /> */}
+      <ToTopButton />
     </div>
   );
 }
