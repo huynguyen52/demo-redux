e28b48ad27965e70ee0724907592b2f25d53c59e
huynguyen <huynguyen5212@gmail.com>
2021-11-15 18:37:55 +0700

    fix headermain.js part3

--- src/components/Header/HeaderMain.js
+++ src/components/Header/HeaderMain.js
@@ -6,7 +6,8 @@ import { useState } from "react/cjs/react.development";
 import Submenu from "./Submenu";
 
 function HeaderMain() {
-  const [page, setPage] = useState("home");
+  // const [page, setPage] = useState("home");
+  const page = "home";
   const [submenu, setSubmenu] = useState("");
   const [showSubnav, setShowSubnav] = useState(false);
 
